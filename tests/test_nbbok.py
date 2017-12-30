eb32532469c7a60818a7805d5f632af6cceadc4a
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2017-12-31 00:05:15 +0100

    got Link class to work

--- tests/test_nbbok.py
+++ tests/test_nbbok.py
@@ -27,15 +27,35 @@ def test_re():
     
     import re
     
-    line = " [ref]: # (Nulla sit amet - chapter 2)\n" 
+    line = " [ref]: # (Nulla sit amet - chapter 2)   extra characters\n" 
 
     pattern = re.compile('.*\[ref\]:[\s#(]*(?P<category>[^-]+)-(?P<desc>[^)]+)')
     m = pattern.match(line)
 
-    print(m)    
-    if m is not None:
-        print(m.groupdict())    
+
+    assert m['category'].strip() == 'Nulla sit amet'
+    assert m['desc'].strip() == 'chapter 2'
+
+
+def test_Header():
+    
+    with pytest.raises(AssertionError):
+        book.Header(" Just some text")
+    
+    
+    line = " ## Heading ABC"
+    h = book.Header(line)
+    
+    
+    assert h.level == 2
+    assert h.txt == 'Heading ABC'
+    
+    link = h.linkTo('test.ipynb')
+    assert link == '[Heading ABC](test.ipynb#Heading ABC)'
+    
+    link = h.linkTo('test.ipynb', indent=2)
+    assert link == '  * [Heading ABC](test.ipynb#Heading ABC)'
 
 if __name__=="__main__":
     
-    test_re()
+    test_exampleDir()
